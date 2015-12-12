59a150584d8b6da1a7f2a2d731b8559cc1b6f36b
weiHelloWorld <wei.herbert.chen@gmail.com>
2015-12-12 15:56:40 -0600

    move test files to src

--- MD_simulation_on_alanine_dipeptide/current_work/src/main_work.py
+++ MD_simulation_on_alanine_dipeptide/current_work/src/main_work.py
@@ -1,7 +1,7 @@
 from ANN_simulation import *
 
 staring_index = 1
-init_iter = iteration(index = staring_index, network = None, num_of_simulation_steps = 3000)
+init_iter = iteration(index = staring_index, network = None, num_of_simulation_steps = 2000)
 
 a = simulation_with_ANN_main(num_of_iterations = 10, initial_iteration = init_iter, training_interval=None)
 a.run_mult_iterations()
