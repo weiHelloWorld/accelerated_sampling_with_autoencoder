cde3808b53e5f099e72a71218cd27ba68845acc1
weiHelloWorld <wei.herbert.chen@gmail.com>
2016-05-01 21:24:37 -0500

    update script for WHAM

--- MD_simulation_on_alanine_dipeptide/current_work/src/temp_for_WHAM.py
+++ MD_simulation_on_alanine_dipeptide/current_work/src/temp_for_WHAM.py
@@ -1,5 +1,15 @@
 from ANN_simulation import *
+import sys
 
-a=pickle.load(open('../resources/network_6.pkl','rb'))
+version = sys.argv[1]
 
-a.generate_files_for_Bayes_WHAM('../target/wham_6/')
+directory_containing_coor_files = '../target/wham/'
+
+a=pickle.load(open('../resources/network_1447.pkl','rb'))
+
+if version == "Bayes":
+	a.generate_files_for_Bayes_WHAM(directory_containing_coor_files)
+elif version == "Standard":
+	a.generate_mat_file_for_WHAM_reweighting(directory_containing_coor_files)
+else:
+	print ('error!')
