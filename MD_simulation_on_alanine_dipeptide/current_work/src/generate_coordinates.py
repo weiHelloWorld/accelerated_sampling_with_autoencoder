192625e506a52abe59103cb57c30c1a4beb968b0
weiHelloWorld <wei.herbert.chen@gmail.com>
2016-06-30 21:32:49 -0500

    1. update workqueue.py: create a file that contains finished jobs,  2. update generate_coordinates.py

--- MD_simulation_on_alanine_dipeptide/current_work/src/generate_coordinates.py
+++ MD_simulation_on_alanine_dipeptide/current_work/src/generate_coordinates.py
@@ -3,8 +3,10 @@ import argparse, subprocess
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--file", type=str, default="", help="specify which pdb file to generate coordinates from")
+parser.add_argument('--exclude_type', type=str, help='specify the molecule type of which the pdb files should be excluded')
 args = parser.parse_args()
 
-
-Alanine_dipeptide().generate_coordinates_from_pdb_files(folder_for_pdb = '../target/Alanine_dipeptide/' + args.file)
-Trp_cage().generate_coordinates_from_pdb_files(folder_for_pdb = '../target/Trp_cage/' + args.file)
+if not args.exclude_type == 'Alanine_dipeptide':
+    Alanine_dipeptide().generate_coordinates_from_pdb_files(folder_for_pdb = '../target/Alanine_dipeptide/' + args.file)
+if not args.exclude_type == "Trp_cage":
+    Trp_cage().generate_coordinates_from_pdb_files(folder_for_pdb = '../target/Trp_cage/' + args.file)
