937d77fedda3cea774e0e005eb4e8557f95a9a21
weiHelloWorld <wei.herbert.chen@gmail.com>
2016-07-05 13:38:21 -0500

    updated: check path before generating coordinates

--- MD_simulation_on_alanine_dipeptide/current_work/src/generate_coordinates.py
+++ MD_simulation_on_alanine_dipeptide/current_work/src/generate_coordinates.py
@@ -1,5 +1,5 @@
 from ANN_simulation import *
-import argparse, subprocess
+import argparse, subprocess, os
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--file", type=str, default="", help="specify which pdb file to generate coordinates from")
@@ -7,6 +7,14 @@ parser.add_argument('--exclude_type', type=str, help='specify the molecule type 
 args = parser.parse_args()
 
 if not args.exclude_type == 'Alanine_dipeptide':
-    Alanine_dipeptide().generate_coordinates_from_pdb_files(folder_for_pdb = '../target/Alanine_dipeptide/' + args.file)
+    temp_path = '../target/Alanine_dipeptide/' + args.file
+    if os.path.exists(temp_path):
+        Alanine_dipeptide().generate_coordinates_from_pdb_files(folder_for_pdb = temp_path)
+    else:
+        print "%s not existed!" % temp_path
 if not args.exclude_type == "Trp_cage":
-    Trp_cage().generate_coordinates_from_pdb_files(folder_for_pdb = '../target/Trp_cage/' + args.file)
+    temp_path = '../target/Trp_cage/' + args.file
+    if os.path.exists(temp_path):
+        Trp_cage().generate_coordinates_from_pdb_files(folder_for_pdb = temp_path)
+    else:
+        print "%s not existed!" % temp_path
