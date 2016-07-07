66e8fb4eb975a887a418b8210403862416684fc7
weiHelloWorld <wei.herbert.chen@gmail.com>
2016-07-07 12:33:59 -0500

    simplified API for generate_coordinates and remove_water_mol

--- MD_simulation_on_alanine_dipeptide/current_work/src/remove_water_mol.py
+++ MD_simulation_on_alanine_dipeptide/current_work/src/remove_water_mol.py
@@ -2,8 +2,8 @@ from ANN_simulation import *
 import argparse, subprocess
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--file", type=str, default="", help="specify which pdb file of which the water molecules need to be removed")
+parser.add_argument("--path", type=str, default="", help="specify the directory/file containing the pdb files")
 args = parser.parse_args()
 
 
-Trp_cage().remove_water_mol_and_Cl_from_pdb_file(folder_for_pdb = '../target/' + args.file)
+Trp_cage().remove_water_mol_and_Cl_from_pdb_file(folder_for_pdb = args.path)
