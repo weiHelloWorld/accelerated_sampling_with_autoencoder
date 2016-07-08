f5cb9050d3dbff736601902e8b4f6a517bda952d
weiHelloWorld <wei.herbert.chen@gmail.com>
2016-07-07 21:00:08 -0500

    fixed generate_coordinates.py

--- MD_simulation_on_alanine_dipeptide/current_work/src/generate_coordinates.py
+++ MD_simulation_on_alanine_dipeptide/current_work/src/generate_coordinates.py
@@ -2,11 +2,19 @@ from ANN_simulation import *
 import argparse, subprocess, os
 
 parser = argparse.ArgumentParser()
+parser.add_argument("mol_type", type=str, help="molecule type of the pdb files")
 parser.add_argument("--path", type=str, default="../target", help="specify the directory/file containing the pdb files")
 args = parser.parse_args()
 
+if args.mol_type == 'Alanine_dipeptide':
+    molecule_type = Alanine_dipeptide()
+elif args.mol_type == 'Trp_cage':
+    molecule_type = Trp_cage()
+else:
+    raise Exception('molecule type error')
+
 temp_path = args.path
 if os.path.exists(temp_path):
-    Trp_cage().generate_coordinates_from_pdb_files(folder_for_pdb = temp_path)
+    molecule_type.generate_coordinates_from_pdb_files(folder_for_pdb = temp_path)
 else:
     print "%s not existed!" % temp_path
