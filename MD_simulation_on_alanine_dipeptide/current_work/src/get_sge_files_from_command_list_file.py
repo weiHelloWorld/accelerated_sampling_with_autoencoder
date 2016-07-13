2ce31324e843ca54e26eaadf171feb9c427b20c4
weiHelloWorld <wei.herbert.chen@gmail.com>
2016-07-13 13:04:40 -0500

    added gpu support for auto_qsub.py and cluster_management.py

---
+++ MD_simulation_on_alanine_dipeptide/current_work/src/get_sge_files_from_command_list_file.py
@@ -0,0 +1,16 @@
+import argparse, subprocess, time
+from cluster_management import *
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("cmdfile", type=str, help="file containing Python programs")
+    parser.add_argument('--gpu', help="whether to run on GPU", action="store_true")
+    parser.add_argument('--folder_sge',type=str, default ='../sge_files/', help='fodler to store sge files')
+    args = parser.parse_args()
+
+    mng = cluster_management()
+    mng.create_sge_files_from_a_file_containing_commands(args.cmdfile, args.folder_sge, run_on_gpu=args.gpu)
+    return
+
+if __name__ == '__main__':
+    main()
