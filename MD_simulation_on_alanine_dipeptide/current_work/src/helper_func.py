93f8cbc63e84772f3935bd7b0a7ed5163d0bdfb3
weiHelloWorld <wei.herbert.chen@gmail.com>
2017-11-22 11:20:09 -0600

    added get_mutual_info_of_two_continuous_vars()

---
+++ MD_simulation_on_alanine_dipeptide/current_work/src/helper_func.py
@@ -0,0 +1,17 @@
+from config import *
+class Helper_func(object):
+    def __init__(self):
+        return
+
+    @staticmethod
+    def get_mutual_info_of_two_continuous_vars(temp_var_0, temp_var_1, bins=10):
+        temp_hist_0, _ = np.histogramdd(temp_var_0, bins=bins)
+        temp_hist_1, _ = np.histogramdd(temp_var_1, bins=bins)
+        temp_hist_2, _ = np.histogramdd(np.array([temp_var_0, temp_var_1]).T, bins=bins)
+        temp_hist_0 /= temp_hist_0.sum()
+        temp_hist_1 /= temp_hist_1.sum()
+        temp_hist_2 /= temp_hist_2.sum()
+        result = np.sum([temp_hist_2[item_x, item_y] * np.log(
+            temp_hist_2[item_x, item_y] / temp_hist_0[item_x] / temp_hist_1[item_y])
+                     for item_x in range(bins) for item_y in range(bins) if temp_hist_2[item_x, item_y] != 0])
+        return result
