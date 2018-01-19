9ac60e52d5e587f5b5f8d103f18292f8860227c6
weiHelloWorld <wei.herbert.chen@gmail.com>
2018-01-19 01:56:16 -0600

    added generate_alkane_residue_code_in_openmm_xml()

--- MD_simulation_on_alanine_dipeptide/current_work/src/helper_func.py
+++ MD_simulation_on_alanine_dipeptide/current_work/src/helper_func.py
@@ -19,3 +19,32 @@ class Helper_func(object):
             entropy_1 = - np.sum(temp_hist_1 * np.log(temp_hist_1))
             result /= (0.5 * (entropy_0 + entropy_1))
         return result
+
+    @staticmethod
+    def generate_alkane_residue_code_in_openmm_xml(num, name):
+        print '''<Residue name="%s">
+<Atom charge="0.09" name="H11" type="HGA3"/>
+<Atom charge="0.09" name="H12" type="HGA3"/>
+<Atom charge="0.09" name="H13" type="HGA3"/>
+<Atom charge="-0.27" name="C1" type="CG331"/>''' % name
+        for item in range(num - 2):
+            print '''<Atom charge="0.09" name="H%d1" type="HGA2"/>
+<Atom charge="0.09" name="H%d2" type="HGA2"/>
+<Atom charge="-0.18" name="C%d" type="CG321"/>''' % (item + 2, item + 2, item + 2)
+        print """<Atom charge="0.09" name="H%d1" type="HGA3"/>
+<Atom charge="0.09" name="H%d2" type="HGA3"/>
+<Atom charge="0.09" name="H%d3" type="HGA3"/>
+<Atom charge="-0.27" name="C%d" type="CG331"/>
+<Bond atomName1="H11" atomName2="C1"/>
+<Bond atomName1="H12" atomName2="C1"/>
+<Bond atomName1="H13" atomName2="C1"/>""" % (num, num, num, num)
+        for item in range(num - 1):
+            print """<Bond atomName1="C%d" atomName2="C%d"/>
+<Bond atomName1="H%d1" atomName2="C%d"/>
+<Bond atomName1="H%d2" atomName2="C%d"/>""" % (item + 1, item + 2, item + 2, item + 2, item + 2, item + 2)
+        print """<Bond atomName1="H%d3" atomName2="C%d"/>
+<AllowPatch name="MET1"/>
+<AllowPatch name="MET2"/>
+</Residue>""" % (num, num)
+        return
+    
