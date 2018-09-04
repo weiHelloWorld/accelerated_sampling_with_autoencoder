d7d2f493114a4e275104c788601b54575fd97639
weiHelloWorld <wei.herbert.chen@gmail.com>
2018-09-04 11:54:33 -0500

    refactor: extract get_data_from_folder() in training

--- MD_simulation_on_alanine_dipeptide/current_work/src/train_network_and_save_for_iter.py
+++ MD_simulation_on_alanine_dipeptide/current_work/src/train_network_and_save_for_iter.py
@@ -21,6 +21,78 @@ parser.add_argument('--auto_scale', type=int, default=False, help="automatically
 parser.add_argument('--save_train_data', type=int, default=False, help="save training data to npy files")
 args = parser.parse_args()
 
+def get_data_from_folder(temp_folder, input_type, output_type):
+    my_coor_data_obj = coordinates_data_files_list(
+        list_of_dir_of_coor_data_files=[temp_folder])
+    coor_data_obj_input = my_coor_data_obj.create_sub_coor_data_files_list_using_filter_conditional(
+        lambda x: not 'aligned' in x)
+    if input_type == 'cossin':
+        data_set = np.array(molecule_type.get_many_cossin_from_coordinates_in_list_of_files(
+            coor_data_obj_input.get_list_of_coor_data_files(), step_interval=args.training_interval))
+    elif input_type == 'Cartesian':
+        scaling_factor = CONFIG_49
+        data_set = coor_data_obj_input.get_coor_data(scaling_factor)
+        data_set = data_set[::args.training_interval]
+        data_set = Sutils.remove_translation(data_set)
+        assert (Sutils.check_center_of_mass_is_at_origin(data_set))
+    elif input_type == 'pairwise_distance':
+        data_set = np.array(Sutils.get_non_repeated_pairwise_distance(
+            coor_data_obj_input.get_list_of_corresponding_pdb_files(), step_interval=args.training_interval,
+            atom_selection=CONFIG_73)) / CONFIG_49
+    else:
+        raise Exception('error input type')
+
+    if output_type == 'cossin':
+        output_data_set = np.array(molecule_type.get_many_cossin_from_coordinates_in_list_of_files(
+            coor_data_obj_input.get_list_of_coor_data_files(), step_interval=args.training_interval))
+    elif output_type == 'Cartesian':
+        scaling_factor = CONFIG_49
+        alignment_coor_file_suffix_list = CONFIG_61
+        output_data_set = Sutils.prepare_output_Cartesian_coor_with_multiple_ref_structures(
+            [temp_folder], alignment_coor_file_suffix_list, scaling_factor)
+        output_data_set = output_data_set[::args.training_interval]
+        mixed_error_function = CONFIG_71  # TODO: refactor this part later
+        if mixed_error_function:
+            if CONFIG_30 == "Trp_cage":
+                output_data_set_1 = Sutils.remove_translation(
+                    output_data_set[:, list(range(9 * 1, 9 * 8))])  # mixed_err
+                output_data_set_2 = Sutils.remove_translation(output_data_set[:, list(range(180, 360))])
+                output_data_set = np.concatenate([4.0 * output_data_set_1, output_data_set_2],
+                                                 axis=1)  # TODO: may modify this relative weight later
+            elif CONFIG_30 == "Src_kinase":
+                output_data_set_1 = Sutils.remove_translation(
+                    output_data_set[:, list(range(9 * 143, 9 * 170))])  # mixed_err
+                output_data_set_2 = Sutils.remove_translation(
+                    output_data_set[:, list(range(2358 + 9 * 43, 2358 + 9 * 58))])
+                output_data_set = np.concatenate([output_data_set_1, output_data_set_2], axis=1)
+        assert (Sutils.check_center_of_mass_is_at_origin(output_data_set))
+    elif output_type == 'pairwise_distance':
+        output_data_set = np.array(Sutils.get_non_repeated_pairwise_distance(
+            coor_data_obj_input.get_list_of_corresponding_pdb_files(), step_interval=args.training_interval,
+            atom_selection=CONFIG_73)) / CONFIG_49
+    elif output_type == 'combined':
+        scaling_factor = CONFIG_49
+        alignment_coor_file_suffix_list = CONFIG_61
+        output_data_set = Sutils.prepare_output_Cartesian_coor_with_multiple_ref_structures(
+            [temp_folder], alignment_coor_file_suffix_list, scaling_factor)
+        output_data_set = output_data_set[::args.training_interval]
+        mixed_error_function = CONFIG_71  # TODO: refactor this part later
+        assert mixed_error_function  # mixed error is required
+        if CONFIG_30 == "Trp_cage":
+            output_data_set_1 = Sutils.remove_translation(output_data_set[:, list(range(9 * 1, 9 * 8))])  # mixed_err
+            output_data_set_2 = Sutils.remove_translation(output_data_set[:, list(range(180, 360))])
+            output_data_set = np.concatenate([4.0 * output_data_set_1, output_data_set_2],
+                                             axis=1)  # TODO: may modify this relative weight later
+        else:
+            raise Exception('not defined')
+        temp_output_data_set = np.array(Sutils.get_non_repeated_pairwise_distance(
+            coor_data_obj_input.get_list_of_corresponding_pdb_files(), step_interval=args.training_interval,
+            atom_selection=CONFIG_73)) / CONFIG_49
+        output_data_set = np.concatenate([output_data_set, temp_output_data_set], axis=1)
+    else:
+        raise Exception('error output data type')
+    return data_set, output_data_set
+
 # used to process additional arguments
 additional_argument_list = {}
 if not args.output_file is None:
@@ -30,91 +102,21 @@ if not args.lr_m is None:
     temp_momentum = float(args.lr_m.strip().split(',')[1])
     additional_argument_list['network_parameters'] = [temp_lr, temp_momentum, 0, True, CONFIG_4[4]]
 
-num_of_copies = args.num_of_copies
 if args.data_folder is None:
-    temp_list_of_directories_contanining_data = ['../target/' + CONFIG_30]
-else:
-    temp_list_of_directories_contanining_data = [args.data_folder]
-
-if (args.in_data is None) and (args.out_data is None):
-    print("data folder is %s" % str(temp_list_of_directories_contanining_data))
-    my_coor_data_obj = coordinates_data_files_list(
-        list_of_dir_of_coor_data_files=temp_list_of_directories_contanining_data)
-    coor_data_obj_input = my_coor_data_obj.create_sub_coor_data_files_list_using_filter_conditional(
-        lambda x: not 'aligned' in x)
+    args.data_folder = '../target/' + CONFIG_30
 
 fraction_of_data_to_be_saved = 1   # save all training data by default
 input_data_type, output_data_type = CONFIG_48, CONFIG_76
 
-# getting input data
+# getting training data
 if not args.in_data is None:
     data_set = np.load(args.in_data)
-elif input_data_type == 'cossin' and output_data_type == 'cossin':  # input type
-    data_set = np.array(molecule_type.get_many_cossin_from_coordinates_in_list_of_files(
-        coor_data_obj_input.get_list_of_coor_data_files(), step_interval=args.training_interval))
-elif input_data_type == 'Cartesian':
-    scaling_factor = CONFIG_49
-    data_set = coor_data_obj_input.get_coor_data(scaling_factor)
-    data_set = data_set[::args.training_interval]
-    data_set = Sutils.remove_translation(data_set)
-    assert (Sutils.check_center_of_mass_is_at_origin(data_set))
-elif input_data_type == 'pairwise_distance':
-    data_set = np.array(Sutils.get_non_repeated_pairwise_distance(
-        coor_data_obj_input.get_list_of_corresponding_pdb_files(), step_interval=args.training_interval,
-        atom_selection=CONFIG_73)) / CONFIG_49
+    if args.out_data is None:
+        output_data_set = data_set
+    else:
+        output_data_set = np.load(args.out_data)
 else:
-    raise Exception('error input type')
-
-# getting output data
-if not args.out_data is None:
-    output_data_set = np.load(args.out_data)
-elif not args.in_data is None:
-    output_data_set = np.load(args.in_data)
-elif output_data_type == 'cossin':   # output type
-    output_data_set = data_set   # done above
-elif output_data_type == 'Cartesian':
-    scaling_factor = CONFIG_49
-    alignment_coor_file_suffix_list = CONFIG_61
-    output_data_set = Sutils.prepare_output_Cartesian_coor_with_multiple_ref_structures(
-        temp_list_of_directories_contanining_data, alignment_coor_file_suffix_list, scaling_factor)
-    output_data_set = output_data_set[::args.training_interval]
-    mixed_error_function = CONFIG_71    # TODO: refactor this part later
-    if mixed_error_function:
-        if CONFIG_30 == "Trp_cage":
-            output_data_set_1 = Sutils.remove_translation(output_data_set[:, list(range(9 * 1, 9 * 8))])  # mixed_err
-            output_data_set_2 = Sutils.remove_translation(output_data_set[:, list(range(180, 360))])
-            output_data_set = np.concatenate([4.0 * output_data_set_1, output_data_set_2],
-                                             axis=1)  # TODO: may modify this relative weight later
-        elif CONFIG_30 == "Src_kinase":
-            output_data_set_1 = Sutils.remove_translation(
-                output_data_set[:, list(range(9 * 143, 9 * 170))])  # mixed_err
-            output_data_set_2 = Sutils.remove_translation(output_data_set[:, list(range(2358 + 9 * 43, 2358 + 9 * 58))])
-            output_data_set = np.concatenate([output_data_set_1, output_data_set_2], axis=1)
-    assert (Sutils.check_center_of_mass_is_at_origin(output_data_set))
-elif output_data_type == 'pairwise_distance':
-    output_data_set = np.array(Sutils.get_non_repeated_pairwise_distance(
-        coor_data_obj_input.get_list_of_corresponding_pdb_files(), step_interval=args.training_interval,
-        atom_selection=CONFIG_73)) / CONFIG_49
-elif output_data_type == 'combined':
-    scaling_factor = CONFIG_49
-    alignment_coor_file_suffix_list = CONFIG_61
-    output_data_set = Sutils.prepare_output_Cartesian_coor_with_multiple_ref_structures(
-        temp_list_of_directories_contanining_data, alignment_coor_file_suffix_list, scaling_factor)
-    output_data_set = output_data_set[::args.training_interval]
-    mixed_error_function = CONFIG_71  # TODO: refactor this part later
-    assert mixed_error_function  # mixed error is required
-    if CONFIG_30 == "Trp_cage":
-        output_data_set_1 = Sutils.remove_translation(output_data_set[:, list(range(9 * 1, 9 * 8))])  # mixed_err
-        output_data_set_2 = Sutils.remove_translation(output_data_set[:, list(range(180, 360))])
-        output_data_set = np.concatenate([4.0 * output_data_set_1, output_data_set_2],
-                                         axis=1)  # TODO: may modify this relative weight later
-    else: raise Exception('not defined')
-    temp_output_data_set = np.array(Sutils.get_non_repeated_pairwise_distance(
-        coor_data_obj_input.get_list_of_corresponding_pdb_files(), step_interval=args.training_interval,
-        atom_selection=CONFIG_73)) / CONFIG_49
-    output_data_set = np.concatenate([output_data_set, temp_output_data_set], axis=1)
-else:
-    raise Exception('error output data type')
+    data_set, output_data_set = get_data_from_folder(args.data_folder, input_data_type, output_data_type)
 
 assert (len(data_set) == len(output_data_set))
 use_representative_points_for_training = CONFIG_58
@@ -123,9 +125,9 @@ if use_representative_points_for_training:
     
 if input_data_type == 'Cartesian' and args.in_data is None:
     print('applying data augmentation...')
-    data_set, output_data_set = Sutils.data_augmentation(data_set, output_data_set, num_of_copies,
+    data_set, output_data_set = Sutils.data_augmentation(data_set, output_data_set, args.num_of_copies,
                              is_output_reconstructed_Cartesian=(output_data_type == 'Cartesian'))
-    fraction_of_data_to_be_saved = 1.0 / num_of_copies
+    fraction_of_data_to_be_saved = 1.0 / args.num_of_copies
 else:
     print("data augmentation not applied")
 
