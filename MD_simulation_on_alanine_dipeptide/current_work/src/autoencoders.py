weiHelloWorld/accelerated_sampling_with_autoencoder

commit 089f1129058945c2ac030bbb8848c7965d2d0e6a

get splitted node before concatenating, fixed tensorflow backend issue 'TypeError: can only concatenate list (not int) to list'

MD_simulation_on_alanine_dipeptide/current_work/src/autoencoders.py
@@ -985,9 +985,9 @@ class autoencoder_Keras(autoencoder):
                 encoded = Reshape((node_num[index_CV_layer],))(x)
                 encoded_split = [temp_lambda_slice_layers_circular[item](encoded) for item in range(num_CVs)]
             else:
-                encoded = Dense(node_num[index_CV_layer], activation=act_funcs[index_CV_layer - 1],
-                                kernel_regularizer=l2(self._network_parameters[4][index_CV_layer - 1]))(x)
-                encoded_split = [temp_lambda_slice_layers[item](encoded) for item in range(num_CVs)]
+                encoded_split = [Dense(1, activation=act_funcs[index_CV_layer - 1],
+                                kernel_regularizer=l2(self._network_parameters[4][index_CV_layer - 1]))(x) for _ in range(num_CVs)]
+                encoded = layers.Concatenate()(encoded_split)
 
             if hierarchical_variant == 0:  # this is logically equivalent to original version by Scholz
                 x_next = [Dense(node_num[index_CV_layer + 1], activation='linear',
@@ -1150,12 +1150,6 @@ temp_lambda_sigmoid_layer = Lambda(lambda x: K.sigmoid(x))
 # not sure if there are better ways to do this, since Lambda layer has to be defined at top level of the file,
 # following line does not work
 # temp_lambda_slice_layers = [Lambda(lambda x: x[:, [index]], output_shape=(1,)) for index in range(20)]
-temp_lambda_slice_layers = [
-    Lambda(lambda x: x[:, [0]], output_shape=(1,)), Lambda(lambda x: x[:, [1]], output_shape=(1,)),
-    Lambda(lambda x: x[:, [2]], output_shape=(1,)), Lambda(lambda x: x[:, [3]], output_shape=(1,)),
-    Lambda(lambda x: x[:, [4]], output_shape=(1,)), Lambda(lambda x: x[:, [5]], output_shape=(1,)),
-    Lambda(lambda x: x[:, [6]], output_shape=(1,)), Lambda(lambda x: x[:, [7]], output_shape=(1,)),
-    Lambda(lambda x: x[:, [8]], output_shape=(1,)), Lambda(lambda x: x[:, [9]], output_shape=(1,))]
 temp_lambda_slice_layers_circular = [
     Lambda(lambda x: x[:, [0,1]], output_shape=(2,)),   Lambda(lambda x: x[:, [2,3]], output_shape=(2,)),
     Lambda(lambda x: x[:, [4,5]], output_shape=(2,)),   Lambda(lambda x: x[:, [6,7]], output_shape=(2,)),

commit a637bcccf25631e7581bb9ab6588d4460f9fa49e

fixed computation of self._connection_between_layers_coeffs, this problem was introduced in commit 089f112, where in order to fix a tensorflow-related issue, I split nodes in CV layer and then concatenate them, instead of creating a complete CV layer and then do split.  Therefore, the dense layer before the CV layer is connected to multiple layers, but in original computation, only first connection is included, which causes problem in biased simulation

MD_simulation_on_alanine_dipeptide/current_work/src/autoencoders.py
@@ -272,6 +272,7 @@ PRINT STRIDE=50 ARG=%s,ave FILE=%s""" % (
 
     def write_coefficients_of_connections_into_file(self, out_file=None):
         if out_file is None: out_file = self._autoencoder_info_file
+        Helper_func.backup_rename_file_if_exists(out_file)
         with open(out_file, 'w') as f_out:
             for item in range(self._index_CV):
                 f_out.write(str(list(self._connection_between_layers_coeffs[item])))
@@ -921,6 +922,13 @@ class autoencoder_Keras(autoencoder):
         actual_output = pca.inverse_transform(pca.fit_transform(data))
         return (1 - np.sum((actual_output - data).var(axis=0)) / np.sum(data.var(axis=0)), pca)
 
+    @staticmethod
+    def get_outbound_layers(layer):
+        """get outbound layers to which this layer is connected,
+        see https://stackoverflow.com/questions/50814880/keras-retrieve-layers-that-the-layer-connected-to"""
+        temp_nodes = layer._outbound_nodes
+        return [item.outbound_layer for item in temp_nodes]
+
     def train(self, hierarchical=None, hierarchical_variant = None):
         act_funcs = [item.lower() for item in self._hidden_layers_type] + [self._out_layer_type.lower()]
         if hierarchical is None: hierarchical = self._hierarchical
@@ -1087,10 +1095,19 @@ parameter = %s, optimizer = %s, hierarchical = %d with variant %d, FVE should no
         [train_in, train_out] = Helper_func.shuffle_multiple_arrays([data, output_data_set])
         train_history = molecule_net.fit(train_in, train_out, epochs=self._epochs, batch_size=self._batch_size,
                                          verbose=False, validation_split=0.2, callbacks=call_back_list)
-        self._connection_between_layers_coeffs = [item.get_weights()[0].T.flatten() for item in
-                                                  encoder_net.layers if isinstance(item, Dense)]  # transpose the weights for consistency
-        self._connection_with_bias_layers_coeffs = [item.get_weights()[1] for item in encoder_net.layers if
-                                                    isinstance(item, Dense)]
+        self._connection_between_layers_coeffs, self._connection_with_bias_layers_coeffs = [], []
+        for item_l in encoder_net.layers:
+            outbound_layers = autoencoder_Keras.get_outbound_layers(item_l)
+            if len(outbound_layers) > 0 and isinstance(outbound_layers[0], Dense):
+                self._connection_between_layers_coeffs.append(
+                    np.concatenate([item_o.get_weights()[0] for item_o in outbound_layers], axis=-1).T.flatten())
+                self._connection_with_bias_layers_coeffs.append(
+                    np.concatenate([item_o.get_weights()[1] for item_o in outbound_layers], axis=-1))
+
+        # self._connection_between_layers_coeffs = [item.get_weights()[0].T.flatten() for item in
+        #                                           encoder_net.layers if isinstance(item, Dense)]  # transpose the weights for consistency
+        # self._connection_with_bias_layers_coeffs = [item.get_weights()[1] for item in encoder_net.layers if
+        #                                             isinstance(item, Dense)]
 
         print(('Done ' + training_print_info + str(datetime.datetime.now())))
         self._molecule_net = molecule_net