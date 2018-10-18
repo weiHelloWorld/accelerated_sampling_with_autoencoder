23d9a946eb3854785053d0555b5703073d806d1f
weiHelloWorld <wei.herbert.chen@gmail.com>
2018-10-18 10:43:16 -0500

    automate hierarchical dynamics encoder learning

---
+++ MD_simulation_on_alanine_dipeptide/current_work/src/hierarchical_dynamics_encoder.py
@@ -0,0 +1,40 @@
+from autoencoders import autoencoder_torch
+from ANN_simulation import machine_independent_run
+from helper_func import *
+import glob
+
+class HDE(object):
+    def __init__(self, data_file, n_iter, lag_time, lr_list, batch_size, epochs, n_models):
+        self._data_file = data_file           # training data file
+        self._n_iter = n_iter                 # number of iterations (i.e. number of slow modes to learn)
+        self._lag_time = lag_time
+        self._lr_list = lr_list               # learning rates to use in each iteration, and then pick the best one
+        self._batch_size = batch_size
+        self._epochs = epochs
+        self._n_models = n_models             # number of models per learning rate
+        return
+
+    def run_iter(self, iter_index, save_CV_for_best_model=True):
+        folder_for_model = "../target/iter_%02d/" % iter_index
+        commands = ["python ../src/train_network_and_save_for_iter.py 1447 --num_of_trainings 1 " \
+                  "--in_data %s --output_file %s/batch_%d_lagtime_%d_lr_%s_index_%02d.pkl " \
+                  "--auto_scale 1 --lr_m %s,0 --node_num 0,100,100,1,100,100,0 --lag_time %d " \
+                  "--rec_loss_type 2 --autocorr_weight 1.0 --save_to_data_files data.npy,data.npy " \
+                  "--batch_size %d" % (
+            self._data_file, folder_for_model, self._batch_size, self._lag_time, item_lr, item_index, item_lr,
+            self._lag_time, self._batch_size) for item_lr in self._lr_list for item_index in range(self._n_models)]
+        machine_independent_run.run_commands(machine_to_run_simulations='local',
+                                             commands=commands, cuda=True, max_num_failed_jobs=1)
+        model_pkls = glob.glob("%s/*.pkl" % folder_for_model)
+        best_model, max_autocorr = None, 0
+        for item in model_pkls:
+            model = autoencoder_torch.load_from_pkl_file(item)
+            CVs = model.get_PCs()
+            autocorr_current = Helper_func.get_autocorr(CVs.flatten(), self._lag_time)
+            if autocorr_current > max_autocorr:
+                max_autocorr = autocorr_current
+                best_model = item
+        if save_CV_for_best_model:
+            model = autoencoder_torch.load_from_pkl_file(best_model)
+            np.save('CV%02d' % iter_index, model.get_PCs())
+        return commands, best_model
