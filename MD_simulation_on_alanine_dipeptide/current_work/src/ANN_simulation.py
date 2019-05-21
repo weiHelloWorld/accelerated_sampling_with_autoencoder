fix some of unit scaling

MD_simulation_on_alanine_dipeptide/current_work/src/ANN_simulation.py
@@ -174,7 +174,7 @@ class plotting(object):
                                 list_of_files=[out_file_name.replace('.pdb', '_coordinates.npy')])
                         elif CONFIG_48 == "Cartesian" or 'pairwise_distance':
                             scaling_factor = CONFIG_49
-                            temp_input_data = np.loadtxt(out_file_name.replace('.pdb', '_coordinates.npy')) / scaling_factor
+                            temp_input_data = np.load(out_file_name.replace('.pdb', '_coordinates.npy')) / scaling_factor
                             temp_input_data = Sutils.remove_translation(temp_input_data)
                         else:
                             raise Exception("input data type error")
@@ -229,7 +229,7 @@ class plotting(object):
         temp_arrow_start_list = []
         _1 = coordinates_data_files_list([coor_file_folder])
         for item in _1.get_list_of_coor_data_files():
-            data = np.loadtxt(item)[starting_index_of_last_few_frames:] / scaling_factor
+            data = np.load(item)[starting_index_of_last_few_frames:] / scaling_factor
             data = Sutils.remove_translation(data)
             potential_centers_list.append([float(item_1) for item_1 in item.split('_pc_[')[1].split(']')[0].split(',')])
             # do analysis using K-S test
@@ -453,7 +453,7 @@ class single_biased_simulation_data(object):
                 [self._file_for_single_biased_simulation_coor]))
         elif input_data_type == 'Cartesian':
             scaling_factor = CONFIG_49
-            temp_data = np.loadtxt(self._file_for_single_biased_simulation_coor) / scaling_factor
+            temp_data = np.load(self._file_for_single_biased_simulation_coor) / scaling_factor
             temp_data = Sutils.remove_translation(temp_data)
             PCs = self._my_network.get_PCs(temp_data)
         else:

MD_simulation_on_alanine_dipeptide/current_work/src/biased_simulation.py
@@ -127,7 +127,8 @@ def run_simulation(force_constant):
             force.set_num_of_nodes(num_of_nodes)
             force.set_potential_center(potential_center)
             force.set_force_constant(float(force_constant))
-            force.set_scaling_factor(float(scaling_factor) / 10.0)     # factor of 10: since default unit is nm in OpenMM
+            unit_scaling = 1.0  # TODO: check unit scaling
+            force.set_scaling_factor(float(scaling_factor) / unit_scaling)   # since default unit is nm in OpenMM
 
             # TODO: need to fix following for multi-hidden layer cases
             temp_coeffs, temp_bias = np.load(autoencoder_info_file)